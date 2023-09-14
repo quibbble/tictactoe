b2f2915afd02eff7f7c99b116d9f4dc508806b96
Chris Fregly <dev@chrisfregly.com>
2023-09-13 19:50:35 -0700

    use quibbble/boardgame components

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # TicTacToe
 
-TicTacToe website that makes use of the quibbble backend. View at https://tictactoe.quibbble.com.
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3929eca4-5a35-498c-a47f-4a666b86e492/deploy-status)](https://app.netlify.com/sites/tictactoe-quibbble/deploys)
+
+TicTacToe game website. Play at [tictactoe.quibbble.com](https://tictactoe.quibbble.com).
+
+This repo contains [ReactJS](https://react.dev) frontend code and makes use of custom React components found at [boardgame](https://github.com/quibbble/boardgame). Game logic can be found at [go-tictactoe](https://github.com/quibbble/go-boardgame/examples/tictactoe). Server logic can be found at [go-quibbble](https://github.com/quibbble/go-quibbble). 
+
+[![Quibbble TicTacToe](screenshot.png)](https://tictactoe.quibbble.com)
+
+## Run Locally
+
+- Generate a personal `GITHUB_ACCESS_TOKEN` with package read permissions. Read more about it [here](https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-npm-registry).
+- Create a `.npmrc` file in the `tictactoe` root directory with the following:
+```
+//npm.pkg.github.com/:_authToken=<GITHUB_ACCESS_TOKEN>
+@quibbble:boardgame=https://npm.pkg.github.com
+```
+- Run `npm i`.
+- Run the quibbble server ([go-quibbble](https://github.com/quibbble/go-quibbble)) locally on port `8080`.
+- Create a `.env.local` file in the `tictactoe` root directory with the following:
+```
+REACT_APP_HOST="http://127.0.0.1:8080"
+REACT_APP_WEBSOCKET="ws://127.0.0.1:8080"
+```
+- Run `npm start`.
---
+++ src/components/game/Game.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState, forwardRef } from "react";
+
+
+export const Game = forwardRef((props, ref) => {
+    // eslint-disable-next-line no-unused-vars
+    const { ws, game, network, chat, connected, error } = props;
+
+    // websocket messages
+    const markLocation = (team, row, col) => {
+        if (!ws.current) return;
+        ws.current.send(JSON.stringify({"ActionType": "MarkLocation", "Team": team, "MoreDetails": {"Row": row, "Column": col}}));
+    }
+
+    // game data
+    const [board, setBoard] = useState([]);
+    useEffect(() => {
+        if (game && game.MoreData) setBoard(game.MoreData.Board)
+    }, [game])
+
+    // network data
+    const [team, setCurrentTeam] = useState("");
+    useEffect(() => {
+        if (network && connected) setCurrentTeam(connected[network.Name])
+    }, [network, connected])
+
+    // board must stay at a 3x3 width to height ratio
+    const [tileSize, setTileSize] = useState(0);
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+    useEffect(() => {
+        function handleResize() {
+            const width = 3;
+            const height = 3;
+            if (!ref || !ref.current) return;
+            if (ref.current.clientHeight/height < ref.current.clientWidth/width) {
+                setWidth(ref.current.clientHeight/height*width);
+                setHeight(ref.current.clientHeight);
+                setTileSize(ref.current.clientHeight/height);
+            } else {
+                setWidth(ref.current.clientWidth);
+                setHeight(ref.current.clientWidth/width*height);
+                setTileSize(ref.current.clientWidth/width);
+            }
+        }
+        handleResize()
+    });
+    useEffect(() => {
+        function handleResize() {
+            const width = 3;
+            const height = 3;
+            if (!ref || !ref.current) return;
+            if (ref.current.clientHeight/height < ref.current.clientWidth/width) {
+                setWidth(ref.current.clientHeight/height*width);
+                setHeight(ref.current.clientHeight);
+                setTileSize(ref.current.clientHeight/height);
+            } else {
+                setWidth(ref.current.clientWidth);
+                setHeight(ref.current.clientWidth/width*height);
+                setTileSize(ref.current.clientWidth/width);
+            }
+        }
+        window.addEventListener("resize", handleResize);
+        return _ => window.removeEventListener("resize", handleResize)
+    }, [ref]);
+
+    return (
+        <div className="w-full h-full flex flex-col justify-center items-center grow">
+            <div className="flex flex-col justify-center overflow-hidden" style={{width: `${width}px`, height: `${height}px`}}>
+            {
+                board.map((row, rIdx) =>
+                    <div key={rIdx} className="w-full flex justify-center" style={{height: `${tileSize}px`}}>
+                        {
+                            row.map((mark, cIdx) =>
+                            <div key={`${rIdx},${cIdx}`} className="flex items-center justify-center cursor-pointer" style={{width: `${tileSize}px`, height: `${tileSize}px`}} onClick={() => markLocation(team, rIdx, cIdx)}>
+                                <div className={`w-full h-full flex flex-col items-center justify-center ${rIdx > 0 ? "border-t" : ""} ${cIdx < 2 ? "border-r" : ""} border-zinc-100 text-6xl font-bold font-sans ${mark === "O" ? `text-red-500` : "text-blue-500"}`}>{ mark.toUpperCase() }</div>
+                            </div>)
+                        }
+                    </div>
+                )
+            }
+            </div>
+        </div>
+    )
+})
